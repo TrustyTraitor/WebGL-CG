6143c169fa7e871bdd34f74a0df0843228540201
Michael Gain <gainmikey@gmail.com>
2024-10-29 18:11:59 -0400

    lab5b

--- Gain-6120-Lab5B/draw.js
+++ Gain-6120-Lab5B/draw.js
@@ -9,22 +9,105 @@ var vertexShader, fragmentShader;
 var delay = 10;
 
 var num_verts = teapot_indices.length;
-
 var vert_colors = [];
+var M_comp = mat4(); 
 
 var vBuffer, cBuffer, iBuffer;
 var vColor, vPosition;
-
 var M_comp_loc;
 
-var M_comp = mat4(); 
+var color_mode_loc;
+var color_mode = 0;
 
 let rcolor = () => [Math.random(), Math.random(), Math.random(), 1.];
 let wcolor = () => [1., 1., 1., 1.];
 
+let current_color
+
 let GenColors = () => {
-	for(let i = 0; i < num_verts; ++i) vert_colors.push(wcolor());
+	for(let i = 0; i < num_verts; ++i) vert_colors.push(rcolor());
 }
+
+function toggleColor() {
+	color_mode = (color_mode+1) % 2;
+}
+
+function lookAt(eye, at, up) {
+	function CalcVPN(eye, at) {
+		return [at[0]-eye[0], at[1]-eye[1], at[2]-eye[2]];
+	}
+
+	let n = normalize(CalcVPN(eye, at), false);
+	let u = normalize(cross_product(n, up), false);
+	let v = normalize(cross_product(u,n), false);
+
+	// let t = negate(eye);
+	// let i = translate4x4(...t);
+	
+	let r = [ [...u, 0], [...v, 0], [...n, 0], [0,0,0,1] ];
+
+	// let V = matMult(r, i);
+	let V = r;
+
+	return V;
+}
+
+function GetBoundingBox() {
+
+	let left = teapot_vertices[0][0];
+	let right = teapot_vertices[0][0];
+	let bottom = teapot_vertices[0][1];
+	let top = teapot_vertices[0][1];
+	let near = teapot_vertices[0][2];
+	let far = teapot_vertices[0][2];
+
+	teapot_vertices.forEach( (e) =>  {
+		left = Math.min(left, e[0]);
+		right = Math.max(right, e[0]);
+
+		bottom = Math.min(bottom, e[1]);
+		top = Math.max(top, e[1]);
+
+		near = Math.min(near, e[2]);
+		far = Math.max(far, e[2]);
+	} );
+
+
+	// This process gives a generous square bounding box
+	let vals = [left, right, bottom, top, near, far];
+	let min = Math.min(...vals);
+	let max = Math.max(...vals);
+
+	return [min, max, min, max, min, max];
+}
+
+function NormOrthoMatrix(left, right, bottom, top, near, far) {
+	let mat = mat4();
+
+	let flip_z = identity4();
+
+	mat = matMult( translate4x4( -(left+right)/2.0, -(bottom+top)/2.0, -(-near+-far)/2.0 ), mat );
+	mat = matMult( scale4x4(2.0/(right-left), 2.0/(top-bottom), 2.0/(far-near)), mat );
+	mat = matMult( flip_z, mat );
+
+	return mat;
+}
+
+function CalcMatrix(eye, at, up) {
+	
+	let look_at = lookAt(eye, at, up);
+	let b_box = GetBoundingBox();
+	let normalize = NormOrthoMatrix(...b_box);
+
+	M_comp = mat4();
+	M_comp = matMult(look_at, M_comp);
+	M_comp = matMult(normalize, M_comp);
+	M_comp = matMult(scale4x4(0.85, 0.85, 0.85), M_comp);
+
+	console.log()
+}
+
+
 
 // all initializations
 window.onload = function init() {
@@ -38,11 +121,12 @@ window.onload = function init() {
 	}
 
 	GenColors();
+	CalcMatrix([0,0,1], [0,0,0], [0,1,1]);
 
 	// set up viewport
 	gl.viewport( 0, 0, canvas.width, canvas.height );
 	gl.clearColor( 0.8, 0.8, 0.0, 1.0 );
-	gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
 	// create shaders, compile and link program
 	program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -76,6 +160,7 @@ window.onload = function init() {
 								 gl.STATIC_DRAW);
 
 	M_comp_loc = gl.getUniformLocation(program, "M_comp");
+	color_mode_loc = gl.getUniformLocation(program, "color_mode");
 
 	// must enable Depth test for 3D viewing in GL
 	gl.enable(gl.DEPTH_TEST);
@@ -88,14 +173,7 @@ let theta = 0;
 function render(){
 	gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	scale = 0.01;
-	theta += 0.01;
-
-	M_comp = mat4();
-	M_comp = matMult(scale4x4(scale,scale,scale), M_comp);
-	M_comp = matMult(rotate4x4(theta, 'y'), M_comp);
-
-
+	gl.uniform1i(color_mode_loc, color_mode);
 	gl.uniformMatrix4fv(M_comp_loc, false, flatten(M_comp));
 	gl.drawElements(gl.TRIANGLES, num_verts, gl.UNSIGNED_SHORT, 0);
 	
